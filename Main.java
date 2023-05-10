a7b420db51fb6a6d8a10b878ba49a6da7f98af49
taimdk <125788374+taimdk@users.noreply.github.com>
2023-05-10 19:54:18 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,117 +1,93 @@
-package bai1;
+package bai2;
+import org.w3c.dom.ls.LSOutput;
+ 
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
+ 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-    static void nhapTK(Account tk) {
-        System.out.println("Nhap so tai khoan : ");
-        tk.setSoTK(sc.nextInt());
+    static void nhapAlbum(Album ab) {
+        System.out.print("Nhập mã CD: ");
+        ab.setMaCD(sc.nextInt());
         sc.nextLine();
-        System.out.println("Nhap ten tai khoan: ");
-        tk.setTenTK(sc.nextLine());
-        tk.setSoTienTrongTK(50);
+        System.out.print("Nhập tên CD : ");
+        ab.setTenCD(sc.nextLine());
+        System.out.print("Nhập tên ca sĩ : ");
+        ab.setCaSy(sc.nextLine());
+        System.out.print("Nhập số lượng bài hát : ");
+        ab.setSoBH(sc.nextInt());
+        System.out.print("Nhập giá thành : ");
+        ab.setGiaThanh(sc.nextFloat());
     }
     public static void main(String[] args) {
-        Account a[] = null;
-        int k, b, n = 0;
-        long s, d, c, f;
+        Album alb[] = null;
+        int a, n = 0;
         boolean flag = true;
         do {
-            System.out.println("Ban chon gi : ");
-            System.out.println("1.them tai khoan\n"
-                    + "2. So tk hien co\n" + "3.In thong tin tk \n" + "4.Nap tien\n"
-                    + "5.Rut tien \n" + "6.Chuyen tien\n" + "7.Chon so khac de thoat");
-            b = sc.nextInt();
-            switch (b) {
+            System.out.println("Bạn chọn làm gì?");
+            System.out.println("1.Nhập thông tin CD \n" +
+                    "2.Xuất danh sách Album.\n" + "3.Tính tổng giá thành \n" + "4.Tổng số lượng CD \n" +
+                    "5.Sắp xếp giảm dần theo giá thành\n"+"6.Sắp xếp tăng dần theo tựa CD\n"+"Nhập số khác để thoát");
+            a = sc.nextInt();
+            switch (a) {
                 case 1:
-                    System.out.println("Them so luong tai khoan ban muon : ");
+                    System.out.println("Nhập số lượng CD : ");
                     n = sc.nextInt();
-                    a = new Account[n];
+                    alb = new Album[n];
                     for (int i = 0; i < n; i++) {
-                        System.out.println("Tai khoan so : " + (i+1));
-                        a[i] = new Account();
-                        nhapTK(a[i]);
+                        System.out.println("CD thứ " + (i + 1)+": ");
+                        alb[i] = new Album();
+                        nhapAlbum(alb[i]);
                     }
                     break;
                 case 2:
-                    System.out.printf("%-10s %-20s %-20s\n", "Số TK", "Tên TK", "So tien co trong tai khoan la ");
+                    System.out.printf("%-10s %-20s %-20s %-10s %-20s \n", "Mã CD", "Tên CD", "Tên ca sỹ", "Số bài hát", "Giá thành");
                     for (int i = 0; i < n; i++) {
-                        a[i].inTK();
+                        alb[i].hienThiAlbum();
                     }
                     break;
                 case 3:
-                   
-                	 System.out.printf("%-10s %-20s %-20s\n", "Số TK", "Tên TK", "So tien co trong tai khoan la ");
-                     for (int i = 0; i < n; i++) {
-                         a[i].inTK();
-                     }
-                     break;
-                case 4:
-                	System.out.println("So tai khoan ban muon in : ");
-                    s = sc.nextLong();
+                    int tong = 0;
                     for (int i = 0; i < n; i++) {
-                        d = a[i].getSoTK();
-                        if (s == d) {
-                            System.out.println("Ban chon tai  khoan: " + d);
-                            a[i].napTien();
-                        } else {
-                            System.out.println("");
-                        }
+                        tong += alb[i].getGiaThanh();
                     }
+                    System.out.println("" +
+                            "Tổng giá thành là : " + tong);
                     break;
-                    
+                case 4:
+                    System.out.println("Tổng số lượng CD là : " + n);
+                    break;
                 case 5:
-                	System.out.println("Nhap so tai khoan ban can nap: ");
-                    s = sc.nextLong();
-                    for (int i = 0; i < n; i++) {
-                        d = a[i].getSoTK();
-                        if (s == d) {
-                            System.out.println("Ban chon tai khoan : " + d);
-                            a[i].rutTien();
+                    Album temp = alb[0];
+                    for (int i = 0; i < n - 1; i++) {
+                        for (int j = i + 1; j < n; j++) {
+                            if (alb[i].getGiaThanh() < alb[j].getGiaThanh()) {
+                                temp = alb[j];
+                                alb[j] = alb[i];
+                                alb[i] = temp;
+                            }
                         }
                     }
+                    System.out.println("Sắp xếp thành công, Nhập số 2 để kiểm tra!");
                     break;
                 case 6:
-                    double chuyen,
-                            nhan,
-                            tienChuyen;
-                    System.out.print("Nhap so khach hang can chuyen tien : ");
-                    s = sc.nextLong();
-                    System.out.print("nhap so khach hang can nhan tien : ");
-                    c = sc.nextLong();
-                    for (int i = 0; i < n; i++) {
-                        d = a[i].getSoTK();
-                        if (s == d) {
-                            chuyen = a[i].getSoTienTrongTK();
-                            for (int j = 0; j < n; j++) {
-                                f = a[j].getSoTK();
-                                if (c == f) {
-                                    nhan = a[j].getSoTienTrongTK();
-                                    System.out.println("Nhap so tien can chuyen:");
-                                    tienChuyen = sc.nextDouble();
-                                    if (tienChuyen <= chuyen) {
-                                        chuyen = chuyen - tienChuyen;
-                                        nhan = nhan + tienChuyen;
-                                        a[i].setSoTienTrongTK(chuyen);
-                                        a[j].setSoTienTrongTK(nhan);
-                                        System.out.println("Tai khoan so " + d + " vua chuyen: $" + tienChuyen);
-                                        System.out.println("Tài khoản số " + f + " vua nhan: $" + tienChuyen);
-                                    } else {
-                                        System.out.println("Số tien nhan khong hop le!");
-                                    }
-                                } else {
-                                    System.out.println("");
-                                }
+                    temp = alb[0];
+                    for (int i = 0; i < n - 1; i++) {
+                        for (int j = i + 1; j < n; j++) {
+                            if (alb[i].getTenCD().compareTo(alb[j].getTenCD())>0) {
+                                temp = alb[j];
+                                alb[j] = alb[i];
+                                alb[i] = temp;
                             }
-                        } else {
-                            System.out.println("");
                         }
                     }
+                    System.out.println("Sắp xếp thành công, Nhập số 2 để kiểm tra!");
                     break;
                 default:
-                    System.out.println("Bye!!");
+                    System.out.println("Goodbye");
                     flag = false;
                     break;
             }
-        } while (flag);
+        }while (flag) ;
     }
 }
